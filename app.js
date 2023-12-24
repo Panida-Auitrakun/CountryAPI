e8e4d7f97f1daca6f136fc7c4a5eba013745d002
Panida Auitrakun <panidaauitrakun1234@gmail.com>
2023-12-24 23:10:45 +0700

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const container = document.getElementById("container")
 const getCountries = async() => {
+    
     const url = 'https://restcountries.com/v3.1/all'
     const res = await fetch(url)
-    const item =  res.json()
-    console.log(item)
+    const item =  await res.json()
+    
+    item.forEach(e => {
+        createCard(e)
+    });
 }
+
+const createCard = (data) => {
+    const cardEl = document.createElement("div")
+    cardEl.classList.add("country")
+    const contentHtml = `
+        <div class="img-container>
+            <img src="${data.flag}"/>
+        </div>
+    `
+}
+getCountries()
